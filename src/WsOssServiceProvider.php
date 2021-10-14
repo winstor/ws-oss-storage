44260632fc94723cd4d7c2e8d65ba79da72e3fc7
winstor <540124900@qq.com>
2021-10-14 10:29:50 +0800

    init

---
+++ src/WsOssServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Winstor\WsOSS;
+
+use Illuminate\Support\ServiceProvider;
+
+class WsOssServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
